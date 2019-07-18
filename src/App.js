5762845bf23af70a94e520bc173e0479b91f5e4b
unknown <devethancraig@gmail.com>
2019-07-18 11:28:00 -0600

    Installed and Created Store && Reducer index.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import AddTechModal from './components/techs/AddTechModal';
 import EditLogModal from './components/logs/EditLogModal';
 import Logs from './components/logs/Logs';
 import SearchBar from './components/layout/SearchBar';
+import store from './store';
 import TechListModal from './components/techs/TechListModal';
 
+import { Provider } from 'react-redux';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -17,18 +19,20 @@ const App = () => {
     M.AutoInit();
   })
   return (
-    <Fragment className="App">
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <AddTechModal />
-        <EditLogModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
-  )
+    <Provider store={store}>
+      <Fragment className="App">
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <AddTechModal />
+          <EditLogModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
 }
 
 export default App;
